389ab0c1e277e9bafbe204772fe3c87a9fa7412e
Ruhaan Bhargav <ruhaanbhargav6@gmail.com>
2025-02-22 10:39:15 -0500

    Customised the modal size and imported it into the index file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Modal, Button, Text } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons'; // Ensure this package is installed
 import Map from '@/components/Map'; // Adjust the path if needed
 import { useTheme } from '../lib/theme/useTheme';  // Adjust the path as needed
 
-
 export default function Index(): JSX.Element {
+
+    // State for the modal visibility
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    // Function to open the modal
+    const openModal = () => {
+      setIsModalVisible(true);
+    };
+  
+    // Function to close the modal
+    const closeModal = () => {
+      setIsModalVisible(false);
+    };
+
   const { theme, toggleTheme } = useTheme();  // Get theme and toggleTheme from context
   return (
     <View style={styles.container}>
       <Map />
       <TouchableOpacity
         style={[styles.startButton, { backgroundColor: theme.buttonBackground }]}
-        onPress={() => console.log('Start button pressed')}
+        onPress={openModal}
       >
         <MaterialIcons name="play-arrow" size={30} color="white" />
       </TouchableOpacity>
+
+      {/* Modal for Changing Profile */}
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Randomize Route</Text>
+      
+            {/* Start Button */}
+            <Button title="Start" onPress={() => console.log("Start Pressed")} />
+      
+            {/* Cancel Button */}
+            <Button title="Cancel" onPress={closeModal} color="red" />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -41,5 +74,24 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 3,
     elevation: 5
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '90%',
+    height: '70%',
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
   },
 });
